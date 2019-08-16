ee57387dedb06e5be44d69fc10a925bcf588d43c
Rafael B. Bentivenha <bentivenha@gmail.com>
2019-08-16 12:22:44 -0300

    problems with git repo

---
+++ app-web/src/App.vue
@@ -0,0 +1,90 @@
+<template>
+  <v-app>
+    <v-container fluid grid-list-lg pa-0 ma-0 justify-center white>
+      <v-layout row cyan accent-3 align-content-center justify-center>
+        <v-flex>
+          <InputForm @clicked="onSend" />
+        </v-flex>
+      </v-layout>
+      <v-container>
+        <v-layout text-center>
+          <HeaderCard :message="message" />
+        </v-layout>
+        <v-layout row justify-center>
+          <v-flex xs7>
+            <DataTable :key="keyTable" v-bind:datatable="employees" />
+          </v-flex>
+          <v-flex xs5>
+            <DoughnutChartContainer
+              :key="keyChart"
+              :loaded="loaded"
+              :labels="employees.names"
+              v-bind:particip="employees.particip"
+            />
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+import DataTable from "./components/DataTable";
+import DoughnutChartContainer from "./components/Doughnut";
+import InputForm from "./components/InputForm";
+import HeaderCard from "./components/HeaderCard";
+import api from "./services/index";
+
+export default {
+  name: "Main",
+  components: {
+    DataTable,
+    DoughnutChartContainer,
+    InputForm,
+    HeaderCard
+  },
+  data() {
+    return {
+      employees: {},
+      loaded: false,
+      error: false,
+      keyChart: 1,
+      keyTable: 1
+    };
+  },
+  watch: {
+    employees: function(newVal) {
+      return newVal;
+    }
+  },
+  computed: {
+    message: function() {
+      if (this.error) return "Something went wrong!";
+      else return null;
+    },
+    key: () => {
+      return 5;
+    }
+  },
+  async mounted() {
+    this.loaded = false;
+    try {
+      const { data } = await api.post("/", {
+        query: `{
+                chartData {
+                  ids
+                  names
+                  lastNames
+                  particip
+                }
+              }`
+      });
+      this.employees = data.data.chartData;
+      this.loaded = true;
+    } catch (e) {
+      this.error = true;
+    }
+  }
+};
+</script>
+
---
+++ app-web/src/components/InputForm.vue
@@ -0,0 +1,97 @@
+<template v-slot:activator="{ on }">
+  <v-form>
+    <v-container mt-3>
+      <v-layout column>
+        <div class="resultContainer">
+          <v-layout align-center justify-center row fill-height>
+            <div class="item">
+              <v-text-field
+                :key="count"
+                v-model="employee.firstName"
+                solo
+                flat
+                label="First Name"
+                required
+              ></v-text-field>
+            </div>
+            <div class="item">
+              <v-text-field
+                :key="count"
+                v-model="employee.lastName"
+                solo
+                flat
+                label="Last Name"
+                required
+              ></v-text-field>
+            </div>
+            <div class="item">
+              <v-text-field
+                :key="count"
+                v-model="employee.participation"
+                solo
+                flat
+                label="Participation"
+                required
+              ></v-text-field>
+            </div>
+            <div class="itemBtn">
+              <ErrorMsg
+                @clicked="onSend"
+                :firstName="employee.firstName"
+                :lastName="employee.lastName"
+                :participation="employee.participation"
+                v-show="true"
+              >Teste</ErrorMsg>
+            </div>
+          </v-layout>
+        </div>
+      </v-layout>
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+import ErrorMsg from "./ErrorMsg";
+
+export default {
+  components: {
+    ErrorMsg
+  },
+  data() {
+    return {
+      employee: {
+        id: "",
+        firstName: "",
+        lastName: "",
+        participation: ""
+      },
+      lista: [],
+      dados: {},
+      count: 0
+    };
+  },
+};
+</script>
+
+<style scoped>
+.resultContainer {
+  height: 100px;
+  margin: auto;
+  width: 100%;
+  padding: 10px;
+}
+
+.item {
+  height: 72px;
+  width: 100%;
+  padding: 10px;
+  margin: auto;
+}
+
+.itemBtn {
+  height: 60px;
+  width: 150px;
+  padding: 0px;
+  margin: auto;
+}
+</style>
